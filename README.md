34e3c5fa33ec8ea098e551956dc9f88134a41a80
mabarraza <mabarraza@miuandes.cl>
2023-06-18 20:32:06 -0400

    Esqueleto y estructura listos

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# ethicapp-website
-Website for the EthicApp Project
+# Ethicapp-website
+
+Página web para el proyecto EthicApp inspirada en la lading page de [Kanaries](https://kanaries.net/home)
+
+## Dependencias
+
+La landing page para ethic app está siendo desarrollada usando las siguientes librerías (que son importadas mediante CDN):
+
+- [Bootstrap 5](https://getbootstrap.com/)
+- [JSCharting](https://jscharting.com/) (para el organigrama del equipo de EthicApp)
+
+## Por hacer
+
+- Agregar responsividad, de momento su correcta visualización esta hecha para notebook (1280x276) que es donde está siendo desarrollada, sin embargo será adaptada para su correcta visualización en desktop, notebook, tablet y celular.
+- Agregar videos/gifs para demostración de características teniendo como ejemplo el sitio web de [Kanaries](https://kanaries.net/home)
+- Agregar estilos a sección desarrollo
+- Agregar footer
+- Obtener testimonios de uso de la aplicación, nombres potenciales:
+  - Yannis Dimitriadis
+  - César Collazos
+  - Davinia Hernandez-Leo
+- Agregar toogle en organigrama para mostrar detalles de cada miembro
+- En sección experiencias cambiar grupo con fotos de organización por slideshow horizontal (carousel)
+- Cambiar borde foto junto a promesa de valor EthicApp
+- Obtener y agregar foto de Vicente Gana para organigrama
---
+++ index.js
@@ -0,0 +1,114 @@
+document.addEventListener('DOMContentLoaded', () => {
+	const chartSeries = [
+		{
+			points: [
+				{
+					id: 'claudio',
+					name: 'Claudio Álvarez',
+					attributes: {
+						position: '<span style="font-size:13px;">BDFL</span>',
+						photo: './assets/team-pictures/claudio_alvarez.jpg',
+					},
+				},
+				{
+					id: 'ignacio',
+					name: 'Ignacio Garcés',
+					parent: 'claudio',
+					attributes: {
+						position: '<span style="font-size:13px;">Mantenedor</span>',
+						photo: './assets/team-pictures/ignacio_garces.jpg',
+					},
+				},
+				{
+					id: 'miguel',
+					name: 'Miguel Barraza',
+					parent: 'ignacio',
+					attributes: {
+						position: '<span style="font-size:13px;">Mantenedor Trainee</span>',
+						photo: './assets/team-pictures/miguel_barraza.jpg',
+					},
+				},
+				{
+					id: 'javier',
+					name: 'Javier Soto',
+					parent: 'miguel',
+					attributes: {
+						position:
+							'<span style="font-size:13px;">Desarrollador Front-End</span>',
+						photo: './assets/team-pictures/javier_soto.jpg',
+					},
+				},
+				{
+					id: 'joaquin',
+					name: 'Joaquín Gracia',
+					parent: 'miguel',
+					attributes: {
+						position:
+							'<span style="font-size:13px;">Desarrollador Back-End</span>',
+						photo: './assets/team-pictures/joaquin_gracia.jpg',
+					},
+				},
+				{
+					id: 'natalia',
+					name: 'Natalia Espinola',
+					parent: 'miguel',
+					attributes: {
+						position:
+							'<span style="font-size:13px;">Desarrolladora Back-End</span>',
+						photo: './assets/team-pictures/natalia_espinola.jpg',
+					},
+				},
+				{
+					id: 'matias',
+					name: 'Matías Rivera',
+					parent: 'miguel',
+					attributes: {
+						position:
+							'<span style="font-size:13px;">Desarrollador Front-End</span>',
+						photo: './assets/team-pictures/matias_rivera.jpg',
+					},
+				},
+				{
+					id: 'vicente',
+					name: 'Vicente Gana',
+					parent: 'miguel',
+					attributes: {
+						position:
+							'<span style="font-size:13px;">Desarrollador Back-End</span>',
+						photo: './assets/team-pictures/claudio_alvarez.jpg',
+					},
+				},
+			],
+		},
+	];
+	const chart = new JSC.Chart('chart_div', {
+		debug: true,
+		type: 'organizational straight right',
+		defaultTooltip_enabled: false,
+		defaultPoint: {
+			focusGlow: false,
+			connectorLine: { width: 1, color: '#e0e0e0' },
+			annotation: {
+				label: {
+					text: '<b>%position</b><br/>%name<br><img margin_left=-55 margin_top=-33 width=50 height=50 src=%photo style="border-radius: 50%">',
+					autoWrap: false,
+					color: 'slategray',
+					style: {
+						fontWeight: 'normal',
+						fontSize: 10,
+					},
+					align: 'left',
+					margin_left: 50,
+				},
+				padding: [4, 14, 4, 0],
+				corners: 'square',
+				height: 52,
+				radius: 33,
+				margin: [2, 15, 2, 0],
+			},
+			outline_color: '#e0e0e0',
+			color: 'white',
+		},
+		series: chartSeries,
+	});
+});
